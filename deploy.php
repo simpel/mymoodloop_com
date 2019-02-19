e1cbbcca0187a028d6e3608f2cb434a441fe0e68
Joel Sandén <me@joelsanden.se>
2019-02-19 09:25:48 +0100

    updated login route

--- deploy.php
+++ deploy.php
@@ -7,6 +7,7 @@ require 'recipe/laravel.php';
 set('application', 'mymoodloop.com');
 
 // Project repository
+//set('repository', 'https://github.com/simpel/mymoodloop_com.git');
 set('repository', 'git@github.com:simpel/mymoodloop_com.git');
 set('branch', 'production');
 
@@ -19,6 +20,8 @@ add('shared_dirs', []);
 
 // Writable dirs by web server
 add('writable_dirs', []);
+
+
 // Hosts
 
 host('mymoodloop.com')
@@ -35,7 +38,6 @@ task('build', function () {
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
 
-
 // Migrate database before symlink new release.
 
-before('deploy:update_code', 'artisan:migrate');
+before('deploy:symlink', 'artisan:migrate');
